7c77a30bbed29c50e094597c1f216e43f89e7528
tk3639 <kalantim5@gmail.com>
2025-03-12 15:09:03 +0100

    Add method by owner

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -16,10 +16,14 @@ import java.text.SimpleDateFormat;
  * month, and year values cannot be changed.
  */
 public class Date {
+
   private final int day;
   private final int month;
   private final int year;
 
+  public void methodAddedByOwner() {
+    System.out.println("Method added by the owner");
+  }
   /**
    * Retrieves the day component of this date.
    *
